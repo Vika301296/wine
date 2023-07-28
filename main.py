52cc7eef575ea3f93d75107d32dc7519a4eb8de8
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-07-28 14:38:57 +0300

    Изменен README.md, добавлена загрузка переменных из окружения, добавлен requierements.txt, оптимизация скрипта

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import collections
 import datetime
 import pandas
+import os
 
+from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-FOUNDATION_YEAR = 1920
-FOUNDATION_MONTH = 1
-FOUNDATION_DAY = 1
-SECONDS_IN_YEAR = 31557600
+load_dotenv()
 
-data_file = 'wine3.xlsx'
-sheet_name = 'Лист1'
+DATA_FILE = os.getenv("DATA_FILE")
+SHEET_NAME = os.getenv("SHEET_NAME")
+FOUNDATION_YEAR = int(os.getenv("FOUNDATION_YEAR"))
+FOUNDATION_MONTH = int(os.getenv("FOUNDATION_MONTH"))
+FOUNDATION_DAY = int(os.getenv("FOUNDATION_DAY"))
+SECONDS_IN_YEAR = int(os.getenv("SECONDS_IN_YEAR"))
+CURRENT_YEAR = datetime.date.today()
 
 
 def read_xlsx(data_file, sheet_name):
     excel_data_df = pandas.read_excel(data_file,
                                       sheet_name=sheet_name,
                                       na_values='nan', keep_default_na=False)
-    drinks = excel_data_df.to_dict(orient='records')
-    drinks_list = collections.defaultdict(list)
-    for drink in drinks:
-        drinks_list[drink['Категория']].append(drink)
-    return drinks_list
+    all_drinks = excel_data_df.to_dict(orient='records')
+    drinks = collections.defaultdict(list)
+    for drink in all_drinks:
+        drinks[drink['Категория']].append(drink)
+    return drinks
 
 
-def year_format(year):
+def format_year(year):
     if year >= 5 and year <= 20:
         return f"{year} лет"
     if year > 100 and (
@@ -41,31 +45,29 @@ def year_format(year):
     return f"{year} лет"
 
 
-def age(foundation_year):
-    current_year = datetime.date.today()
+def count_age(foundation_year, current_year):
     foundation_year = datetime.date(
         year=FOUNDATION_YEAR, month=FOUNDATION_MONTH, day=FOUNDATION_DAY
     )
     age = int(
         (current_year - foundation_year).total_seconds() / SECONDS_IN_YEAR)
-    return year_format(age)
+    return format_year(age)
 
 
-env = Environment(loader=FileSystemLoader("."),
-                  autoescape=select_autoescape(["html"]))
+if __name__ == '__main__':
+    env = Environment(loader=FileSystemLoader("."),
+                      autoescape=select_autoescape(["html"]))
+    template = env.get_template("template.html")
 
-template = env.get_template("template.html")
+    drinks_list = read_xlsx(DATA_FILE, SHEET_NAME)
 
-drinks_list = read_xlsx(data_file, sheet_name)
+    rendered_page = template.render(
+        age=count_age(FOUNDATION_YEAR, CURRENT_YEAR),
+        drinks=drinks_list
+        )
 
-rendered_page = template.render(
-    age=age(FOUNDATION_YEAR),
-    drinks=drinks_list
-    )
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
-
-
-server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
